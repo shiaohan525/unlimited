6e0a50c63b0862f605ab8652578bc7270f108e06
ShiaoHan <shiaohan@mengnanrili.local>
2024-06-06 18:37:51 +0800

    deploy

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import TheHeader from '@/components/TheHeader.vue';
+</script>
+
+<template>
+  <TheHeader/>
+  <RouterView />
+</template>
+
+<style lang="scss" scoped>
+</style>
