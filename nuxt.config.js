7dcc7ef0ed570fdc60303169043be7a9c06edd9c
HsiaoHan <o134888@gmail.com>
2024-09-22 23:47:57 +0800

    新增SEO標籤

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,11 +16,56 @@ export default defineNuxtConfig({
   },
   app: {
     baseURL: '/',
-    buildAssetsDir: '/static/'
+    buildAssetsDir: '/static/',
+    //SEO meta
+    head: {
+      title: "The Unlimited｜HsiaoHan's Style：Web、Design And Future",
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            '致力為品牌打造獨一無二的設計！提供平面與網頁設計服務，分享設計理念和開發心路歷程'
+        },
+        // { hid: 'keywords', name: 'keywords', content: 'keyword1, keyword2, keyword3' },
+        { name: 'robots', content: '平面設計,商業設計, 網頁設計, 印刷輸出' },
+        { name: 'author', content: 'HsiaoHan Hsu' },
+        {
+          property: 'og:title',
+          content: "The Unlimited｜HsiaoHan's Style：Web、Design And Future"
+        },
+        {
+          property: 'og:description',
+          content:
+            '致力為品牌打造獨一無二的設計！提供平面與網頁設計服務，分享設計理念和開發心路歷程'
+        },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://theunlimited.cc/' },
+        { property: 'og:image', content: 'https://theunlimited.cc/images/og_image.png' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        {
+          name: 'twitter:title',
+          content: "The Unlimited｜HsiaoHan's Style：Web、Design And Future"
+        },
+        {
+          name: 'twitter:description',
+          content:
+            '致力為品牌打造獨一無二的設計！提供平面與網頁設計服務，分享設計理念和開發心路歷程'
+        },
+        { name: 'twitter:image', content: 'https://theunlimited.cc/images/twitter_image.png' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'canonical', href: 'https://theunlimited.cc/' }
+      ]
+    }
   },
   router: {
     base: '/'
   },
-  ssr: true, // 确保启用了服务器端渲染
-  plugins: ['@/plugins/vue-gtag.js', '@/plugins/vue-gtm.js', '@/plugins/fz-bao-you.js']
+  ssr: true, // 服務器端渲染
+  plugins: ['@/plugins/vue-gtag.js', '@/plugins/vue-gtm.js', '@/plugins/fz-bao-you.js'],
+  debug: true // 日誌測試
 })
